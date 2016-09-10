b3f26b5847f1671a1f4d5c9a5e097afe2c6e8019
David Jeusette <jeusette.david@gmail.com>
2016-09-10 23:27:11 +0200

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,18 @@
+FROM openjdk:8-jre
+
+ENV HOME /home/graphhopper
+
+ENV GRAPHHOPPER_URL https://oss.sonatype.org/content/groups/public/com/graphhopper/graphhopper-web/0.7.0/graphhopper-web-0.7.0-bin.zip
+
+RUN wget $GRAPHHOPPER_URL -P /tmp && unzip /tmp/graphhopper*.zip -d $HOME && rm /tmp/*.zip
+
+ADD scripts $HOME
+ADD properties $HOME
+
+VOLUME ["/data"]
+
+EXPOSE 8989
+
+WORKDIR $HOME
+
+CMD ["./start.sh"]
---
+++ scripts/start.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+if [ -z "$OSM_FILE" ]; then
+  OSM_FILE=`ls /data/*.pbf`
+fi
+
+if [ -z "${JAVA_OPTS}" ]; then
+    JAVA_OPTS="$JAVA_OPTS -Xms64m -Xmx1024m -XX:MaxPermSize=256m -Djava.net.preferIPv4Stack=true"
+    JAVA_OPTS="$JAVA_OPTS -server -Djava.awt.headless=true -Xconcurrentio"
+    echo "Setting default JAVA_OPTS"
+fi
+
+RUN_ARGS=" -jar $HOME/*.jar jetty.resourcebase=$HOME/webapp config=$HOME/config.properties osmreader.osm=$OSM_FILE"
+
+echo "JAVA_OPTS= ${JAVA_OPTS}"
+echo "RUN_ARGS= ${RUN_ARGS}"
+
+java $JAVA_OPTS $RUN_ARGS
